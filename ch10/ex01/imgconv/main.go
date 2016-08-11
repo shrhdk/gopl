2a37c2ea1969a6fa7ac2214176f584181cc36f56
Hideki Shiro <hideki@shiro.be>
2016-08-12 01:03:14 +0900

    add 10.1

---
+++ ch10/ex01/imgconv/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"log"
+	"os"
+)
+
+// go build gopl.shiro.be/ch10/ex01/imgconv
+// cat gopher.jpg | ./imgconv -t png > gopher.png
+func main() {
+	format := flag.String("t", "jpeg", "[jpg|jpeg|png|gif]")
+	flag.Parse()
+
+	img, _, err := image.Decode(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch *format {
+	case "jpg", "jpeg":
+		err = jpeg.Encode(os.Stdout, img, &jpeg.Options{Quality: 95})
+	case "png":
+		err = png.Encode(os.Stdout, img)
+	case "gif":
+		err = gif.Encode(os.Stdout, img, nil)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown format: %s\n", *format)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
