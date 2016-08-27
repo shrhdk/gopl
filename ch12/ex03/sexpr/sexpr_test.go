5a084fa052c96fac6ebd8bf500d81d1d5b5b8ac6
Hideki Shiro <hideki@shiro.be>
2016-08-28 00:01:04 +0900

    add 12.3

---
+++ ch12/ex03/sexpr/sexpr_test.go
@@ -0,0 +1,81 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package sexpr
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+// Test verifies that encoding and decoding a complex data value
+// produces an equal result.
+//
+// The test does not make direct assertions about the encoded output
+// because the output depends on map iteration order, which is
+// nondeterministic.  The output of the t.Log statements can be
+// inspected by running the test with the -v flag:
+//
+// 	$ go test -v gopl.io/ch12/sexpr
+//
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+}
+
+func TestEncode(t *testing.T) {
+	tests := []struct {
+		given interface{}
+		want  string
+	}{
+		{true, "t"},
+		{false, "nil"},
+		{1.0, "1.000000"},
+		{1 + 2i, "#C(1.000000 2.000000)"},
+	}
+
+	for _, test := range tests {
+		var buf bytes.Buffer
+		err := encode(&buf, reflect.ValueOf(test.given))
+		if err != nil {
+			t.Errorf("error occured while encoding %v. %v", test.given, err)
+			continue
+		}
+
+		if got := buf.String(); got != test.want {
+			t.Errorf("encode(%v), got %v, want %v", test.given, got, test.want)
+		}
+	}
+}
